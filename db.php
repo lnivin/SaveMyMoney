75c938b0f404356d3896d1b320933ecdccce34b7
lnivin <liannivins@gmail.com>
2021-01-28 06:38:05 -0500

    Add remote database configuration

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "127.0.0.1";
-$username = "test";
-$password = "pulpit02";
-$dbname = "test_db";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
