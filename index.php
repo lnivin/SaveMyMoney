52df5b00c8f9669682376cb932f929392ae07eb3
lnivin <liannivins@gmail.com>
2021-01-28 07:29:25 -0500

    Agregar etiqueta para Google Analytics

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-NCVS4D36R6"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NCVS4D36R6');
+  </script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Save My Money</title>
